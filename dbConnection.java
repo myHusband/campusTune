f5eb623b34f673ce390a46fc49a37c651aa4e4b3
Jossephat M Feruzi <mutagwaba72@gmail.com>
2016-12-19 18:48:44 +0200

    Add files via upload

--- dbConnection.java
+++ dbConnection.java
@@ -1,4 +1,4 @@
-package cpe513;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 import java.sql.*;
+import java.util.Vector;
 
 public class dbConnection {
 	Connection conn  ;
@@ -16,26 +17,16 @@ public class dbConnection {
 	String userText= null;
 	String Password= null;
 	
+
 	 
-	auxillaryClass aux2 = new auxillaryClass();
+	
 	
  
     
     
-    public void getDataFromMysql(String username, char[] pass) throws SQLException, ClassNotFoundException{
+    public void getDataFromMysql() throws SQLException, ClassNotFoundException{
     	
-    	if(username.equals(""))
-			JOptionPane.showMessageDialog(null,"Please Enter Your username","ERROR_ON_USERNAME",JOptionPane.ERROR_MESSAGE);
-		
-		else
-		{
-			userText  = username;
-			if(pass.length==0)
-				JOptionPane.showMessageDialog(null,"Please enter Your valid Password","ERROR_ON_PASSWORD",JOptionPane.ERROR_MESSAGE);
-			else
-			{  
-				Password = String.copyValueOf(pass);
-				
+    	
 				Class.forName("com.mysql.jdbc.Driver");
 	    		conn = DriverManager.getConnection("jdbc:mysql://localhost/campusTune","root", "");
 	    		System.out.print("Database is connected !\n\n");
@@ -43,45 +34,17 @@ public class dbConnection {
 	    		stmt = conn.createStatement();
 	    		String sql = "SELECT username,password FROM eusers";
 	    		ResultSet result =stmt.executeQuery(sql);
-	    		//ResultSetMetaData rsdm = result.getMetaData();
-	    		
+	    		   		
 	    		while(result.next()){
-	    			String user = result.getString(1);
-	    			String passwd= result.getString(2);
-	    			
-	    			if(username.equals(user) && Password.equals(passwd)){
-	    				new ProductForm();
-	    				break;
-	    			}
-	    			else 
-	    				if(username.equals(user) && Password.equals(passwd) && username.equals("admin") &&  Password.equals("admin")){
-		    				AdminPaneL ad = new AdminPaneL();
-		    				ad.setVisible(true);
-		    				break;
-		    			}
-	    			else {JOptionPane.showMessageDialog(null, "Invalid user with username " + username);}
-	    		}
+	    			this.userText = result.getString(1);
+	    			this.Password = result.getString(2);
+	    			//System.out.println(user + " "+ passwd);
 	    		
-	    		
-				
-				
-			}
-		}
+	    		}	
          
          stmt.close();
     	conn.close();
     	
     }
     
-    public static void main(String[] args){
-    	//createConnection();
-    	//insertDataToMysql();
-    }
-
-	
-
-
-	
-    
-
 }
