01c7cacf2e6f8a4714283cad73c9aa2872e1d5e3
Jossephat M Feruzi <mutagwaba72@gmail.com>
2016-12-20 14:20:15 +0200

    Add files via upload

--- ProductForm.java
+++ ProductForm.java
@@ -1,5 +1,27 @@
 package cpe513;
 
-public class ProductForm {
+import javax.swing.JFrame;
 
+public class ProductForm extends JFrame {
+
+	
+	
+	public ProductForm() {
+		// TODO Auto-generated constructor stub
+		
+		setTitle("Product Panel");
+		setLayout(null);
+		setSize(600,450);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ProductForm();
+
+	}
 }
