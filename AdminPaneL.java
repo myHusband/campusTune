52c72d26919802085470032cf78f268ca3c783c1
Jossephat M Feruzi <mutagwaba72@gmail.com>
2016-12-25 03:14:31 +0200

    Update AdminPaneL.java

--- AdminPaneL.java
+++ AdminPaneL.java
@@ -1,63 +1,157 @@
 package cpe513;
+package cpe513;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.*;
-import java.sql.SQLException;
-import java.util.Vector;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class AdminPaneL extends JFrame implements ActionListener {
+public class AdminPanel  extends JFrame implements ActionListener{
 	
-
-	JButton submitButton = new JButton("submit");
-	JButton clearButton = new JButton("clear");
+	JPanel headerPanel = new JPanel();
+	JPanel contentPanel = new JPanel();
+	ImageIcon icon = new ImageIcon("C:\\Users\\Mutag\\workspace\\cpe513\\images\\Admin.png");
+	
+	JButton viewProduct = new JButton("viewAllProduct");
+	JButton addProduct = new JButton("addProduct");
+	JButton removeProduct = new JButton("removeProduct");
+	JButton updateProduct = new JButton("updateProduct");
+	JButton deleteProduct = new JButton("deleteProduct");
+	
+	JLabel headerLabel = new JLabel("");
+	JLabel secondheaderLabel = new JLabel("");
+	JLabel productLabel = new JLabel("Enter Your Productc Here :");
+	JTextField productText = new JTextField(20);
+	JLabel headerTableLabel = new JLabel("");
+	JLabel contentLabel = new JLabel("");
+	
+	JTable productTable = new JTable();
 	
 	
-
-	
-	public AdminPaneL() {
+	public AdminPanel() {
 		// TODO Auto-generated constructor stub
-		setTitle("Admin Panel");
+		pack();
+		setTitle("Admin ");
 		setLayout(null);
-		setSize(600,450);
+		setSize(630,500);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		//pack();
-	
-	 	 
-	 	submitButton.setBounds(200, 350, 80, 20);
-	 	add(submitButton);
-	 	submitButton.addActionListener(this);
-	 	
-	 	clearButton.setBounds(285, 350, 70, 20);
-	 	add(clearButton);
-	 	clearButton.addActionListener(this);
-	 	this.getContentPane().setBackground(Color.gray);
 		
+		
+	  // headerPanel.add(icon);
+	  
+	   headerPanel.setBounds(0, 0, 630, 110);
+       add(headerPanel);
+       headerPanel.setLayout(null);
+       headerPanel.setBackground(Color.white);
+       
+       headerLabel.setBounds(470, 0, 160, 130);
+       headerPanel.add(headerLabel);
+       headerLabel.setIcon(icon);
+       
+       secondheaderLabel.setBounds(120, 0, 320, 110);
+       headerPanel.add(secondheaderLabel);
+       secondheaderLabel.setText("Manage your Product From here ");
+       secondheaderLabel.setFont(new Font("Gungsuh", Font.PLAIN, 22));
+       //secondheaderLabel.setIcon(icon);
+       
+       
+       
+       contentPanel.setBounds(450, 150, 170, 300);
+       add(contentPanel);
+       contentPanel.setLayout(null);
+       contentPanel.setBackground(Color.gray);
+       contentPanel.add(viewProduct);
+       contentPanel.add(addProduct);
+       contentPanel.add(removeProduct);
+       contentPanel.add(deleteProduct);
+       
+       
+       viewProduct.setBounds(465, 200, 130,25);
+ 	   add(viewProduct);
+ 	   viewProduct.addActionListener(this);
+ 	   
+ 	  addProduct.setBounds(465, 227, 130,25);
+	   add(addProduct);
+	  addProduct.addActionListener(this);
+	   
+	   updateProduct.setBounds(465, 255, 130,25);
+	   add( updateProduct);
+	   updateProduct.addActionListener(this);
+	   
+	   deleteProduct.setBounds(465, 285, 130,25);
+	   add( deleteProduct);
+	  deleteProduct.addActionListener(this);
+ 	   
+       
+       
+       headerTableLabel.setBounds(170, 115, 400, 30);
+       add(headerTableLabel);
+       headerTableLabel.setText("View Your Product here");
+       headerTableLabel.setForeground(Color.blue);
+       
+       contentLabel.setBounds(470, 115, 200, 30);
+       add(contentLabel);
+       contentLabel.setText("Manage Content");
+       contentLabel.setForeground(Color.blue);
+       
+       
+       
+       
+      
+    
+       
+       JScrollPane js = new JScrollPane(productTable);
+       js.setBounds(0, 150, 468, 300);
+       js.setPreferredSize(new Dimension (468,80));
+       js.setBackground(Color.pink);
+       add(js);
+       
+
+		
+ 	 // this.getContentPane().setBackground(Color.WHITE);
 		setVisible(true);
+		
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new AdminPaneL();
-
-	}
-
 	
+	
+
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		
-		if (e.getSource()== clearButton){dispose();new AdminPaneL();}
-		
-		if(e.getSource() ==   submitButton){
+		if(arg0.getSource() == viewProduct){
 			
-			  
+			
 		}
 		
+      if(arg0.getSource() == addProduct){
+    	  dispose();
+    	  new addProduct();
+			
+		}
+      
+      if(arg0.getSource() == deleteProduct){
+    	  dispose();
+    	  new deleteProduct();
+			
+		}
+      if(arg0.getSource() == updateProduct){
+    	  dispose();
+    	  new updateProduct();
+		}
+      
 		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new AdminPanel();
+
 	}
 
 }
+
